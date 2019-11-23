48f5764b453431284de7760c3a073022df94881c
IOS Developer <ios.dev@adeption.io>
2019-11-23 06:20:17 +0530

    Task5 completed

---
+++ Task_6/Task_6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task_6
+//
+//  Created by Thanos on 23/11/19.
+//  Copyright © 2019 Thanos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
